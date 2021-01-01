469604b0752f3c53b2191d49d257eca6afc56f1c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-12-31 20:44:08 -0800

    kernel: Boost CPU for a short amount of time when zygote forks
    There is a gap from when a new app is forked to the time that the CPU
    can pick up the increased load and ramp up appropriately. Boosting the CPU
    for 50 ms upon a fork from zygote covers that gap, speeding up cold app launches.
    
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    [cyberknight777: port over [1] to mtk]
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    [1]: https://github.com/Neternels/android_kernel_xiaomi_mojito/commit/49b121b4fd92b29dbba1ebce380a2143a6a30df5
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Egii <regidesoftian@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -96,6 +96,7 @@
 #include <linux/scs.h>
 #include <linux/simple_lmk.h>
 
+#include <linux/cpu_input_boost.h>
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
 #include <linux/uaccess.h>
@@ -2259,6 +2260,10 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
+
+	/* Boost CPU to the max for 50 ms when userspace launches an app */
+	if (task_is_zygote(current))
+		cpu_input_boost_kick_max(50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
